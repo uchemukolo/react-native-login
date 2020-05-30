e3935158577620aa28fa4739a8e7aee01d39b43b
uchemukolo <uche.mukolo@jjj.work>
2020-05-30 22:51:11 +0100

    add login form

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <KeyboardAvoidingView behavior='null' style={styles.container}>
+    <View>
+    <Text style={styles.logo}>Pulse</Text>
+    <Text style={styles.welcome}>Welcome to Pulse, Please Login</Text>
+   
+      <View style={styles.subContainer}>
+        <TextInput style={styles.input} placeholder='username 0r email' />
+        <TextInput style={styles.input} placeholder='password' />
+        <TouchableOpacity style={styles.button}> 
+          <Text>Login</Text>
+          </TouchableOpacity>
+      </View>
+      </View>
+    </KeyboardAvoidingView> 
   );
 }
 
@@ -21,16 +26,44 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 20,
+    color: '#f3f3f5',
+    backgroundColor: '#f3f3f5',
   },
-  welcome: {
-    fontSize: 20,
+  subContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
+    color: '#11be9f',
+    fontWeight: 'bold',
+   
   },
-  instructions: {
+  welcome: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#8DC7B9',
     marginBottom: 5,
   },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: '#dbe5e3',
+    backgroundColor: '#e4efed',
+    borderWidth: 1,
+    marginBottom: 20
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: "#8DC7B9",
+    padding: 10,
+    width: 300,
+    height: 60,
+    fontWeight: 'bold',
+    borderRadius: 5,
+  }
 });
